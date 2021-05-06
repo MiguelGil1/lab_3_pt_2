8979f540495d5a44c5d90b9b310e8705359b718a
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-05-05 23:23:10 -0500

    Se agrego la funcionalidad de no poder agregar usuarios ya registrados

--- sudo.cpp
+++ sudo.cpp
@@ -1,12 +1,16 @@
 #include "sudo.h"
 #include "encriptar_desencriptar.h"
 #include <fstream>
-
+#include <string.h>
+#include <stdio.h>
+#include <cstdlib>
 
 
 void sudo::verifying_password(){
     const int semilla = 4, metodo = 2;
     codificar_decodificar dato;
+    ifstream Leer;
+    ofstream Guardar, Temp;
     string clave_admin = "";
     cout << "Bienvenido Administrador." << endl;
     cout << "Por favor ingrese la clave: ";
@@ -26,27 +30,71 @@ void sudo::verifying_password(){
             case 1:{
                 system("cls");
                 ofstream Guardar;
-                string id = "", clave = "", saldo = "", usuario = "";
+                string id_usuario = "", clave_usuario = "", saldo = "", usuario = "";
                 cout << "Registro de usuarios." << endl;
                 cout << "Ingrese identificacion: ";
-                cin >> id;
-                usuario += id+",";
-                cout << "Ingrese clave (4 digitos): ";
-                cin >> clave;
-                while(int(clave.length()) != 4){
-                    cout << "La clave debe ser de 4 digitos." << endl;
-                    cout << "Ingrese clave (4 digitos): ";
-                    cin >> clave;
-                }
-                usuario += clave + ",";
-                cout << "Ingrese el saldo: ";
-                cin >> saldo;
-                usuario += saldo + "\n";
-                string usuario_codificado = dato.codificar(semilla,metodo,usuario);
-                Guardar.open("../usuarios.txt",ios::app);
-                Guardar << usuario_codificado;
+                cin >> id_usuario;
+
+                string contendido_archivo= dato.decodificar(semilla,metodo,"usuarios");
+                string id_tmp = "";
+                int saldo_transaccional = 0, clave = 0;
+                bool encontrado = false;
+
+                Guardar.open("../usuarios_decodificados.txt");
+                Guardar << contendido_archivo;
                 Guardar.close();
-                cout << "Usuario creado correctamente!" << endl;
+
+                Leer.open("../usuarios_decodificados.txt");
+                char linea[300];
+                Leer.getline(linea,sizeof(linea));
+                char id[15];
+                while(!Leer.eof()){
+                    for(int i = 0; i < 3; i++){
+                        char *puntero;
+                        if(i == 0){
+                            puntero = strtok(linea, ",");
+                            strcpy(id, puntero);
+                            id_tmp = id;
+                        }else if(i == 1){
+                            puntero = strtok(NULL,",");
+                            clave = atoi(puntero);
+                        }else if(i == 2){
+                            puntero = strtok(NULL,"\n");
+                            saldo_transaccional = atoi(puntero);
+                        }
+
+                    }
+                    if(id_usuario == id_tmp){
+                        encontrado = true;
+                        break;
+                    }else{
+                        Leer.getline(linea,sizeof(linea));
+                    }
+
+                }
+                Leer.close();
+                if(encontrado == false){
+                    usuario += id_usuario+",";
+                    cout << "Ingrese clave (4 digitos): ";
+                    cin >> clave_usuario;
+                    while(int(clave_usuario.length()) != 4){
+                        cout << "La clave debe ser de 4 digitos." << endl;
+                        cout << "Ingrese clave (4 digitos): ";
+                        cin >> clave;
+                    }
+                    usuario += clave_usuario + ",";
+                    cout << "Ingrese el saldo: ";
+                    cin >> saldo;
+                    usuario += saldo + "\n";
+                    string usuario_codificado = dato.codificar(semilla,metodo,usuario);
+                    Guardar.open("../usuarios.txt",ios::app);
+                    Guardar << usuario_codificado;
+                    Guardar.close();
+                    cout << "Usuario creado correctamente!" << endl;
+                }else{
+                    cout << "Usuario ya existente en la base de datos." << endl;
+                }
+                remove("../usuarios_decodificados.txt");
                 break;
             }
             case 2:
--- sudo.h
+++ sudo.h
@@ -1,3 +1,21 @@
+/*
+    CLAVE DE USUARIO ADMINISTRADOR: 1001249237
+    En esta libreria se tiene una clase llamada sudo, la cual cuanta con una funcion
+    publica tipo void llamada verifying_password la cual no tiene ningun parametro
+    de entrada.
+    Condiciones de uso:
+    -> Esta funcion tiene como principal objetivo la verificacion de la clave administrador
+       por lo que al invocarla, le pide al usuario que ingrese la clave
+       Por lo cual lo que se procede a hacer es a pedir por pantalla la la contrasena
+       , luego se lee el archivo sudo.txt que debe estar unicado en esta direccion relativa:
+
+       ../sudo.txt
+
+       se decodifica la informacion alli almacenada y se compara con la clave ingresada
+       si son iguales se le da acceso al usuario admistrador para poder registrar usuarios
+       en el banco
+       De lo contrario se imprimira el mensaje de clave incorrecta
+*/
 #ifndef SUDO_H
 #define SUDO_H
 #include <iostream>
@@ -6,7 +24,6 @@ using namespace std;
 class sudo{
 public:
     void verifying_password();
-
 };
 
 #endif // SUDO_H
