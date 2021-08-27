f67017d831a2fd3831d070f7a2e63603879a3d6f
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-08-26 21:47:16 -0500

    Se realiza la finalizacion de la docuemntacion del archivo sudo.cpp y sudo.h

--- sudo.h
+++ sudo.h
@@ -22,20 +22,41 @@
 #define SUDO_H
 #include <iostream>
 #include "encriptar_desencriptar.h"
+#include <fstream>
 using namespace std;
 
 class sudo{
 public:
-    void verifying_password();
+    void verifying_password();//Verifica si la clave ingresada por sudo concuerda
+    //con la que hay enel archivo
+    void verifying_existence();//Verifica si el suario a registrar existe en la db del ATM
+    void sign_in();//Metodo que se encarga de registrar en la db del ATM al usuario
+
+    //Metodo setter y getter de los atributos privados
     string getClave_sudo_ingresada() const;
     void setClave_sudo_ingresada(const string &value);
+
     bool getKey_clave() const;
     void setKey_clave(bool value);
 
+    string getId_a_registrar() const;
+    void setId_a_registrar(const string &value);
+
+    bool getEncontrado() const;
+    void setEncontrado(bool value);
+
+    string getUsuario_a_registrar() const;
+    void setUsuario_a_registrar(const string &value);
+
 private:
+    //Se crea objeto codificar_decodificar a utulizar en esta clase
     codificar_decodificar lib_codificar;
-    string clave_sudo_ingresada;
-    bool key_clave;
+
+    string clave_sudo_ingresada;//Almacena un string con la clave que ingresa el usuario sudo
+    string id_a_registrar;//String que almacena elid a registrar en la db
+    string usuario_a_registrar;//String quer almacena la info del usuario que se va a registrar
+    bool key_clave;//Booleano que almacena true si las claves suo ingresada y la clave decodificada coinciden, de lo contrario, almacena false
+    bool encontrado;//Bool que almacena true si se encientra el usario, de lo contrario false
 };
 
 #endif // SUDO_H
